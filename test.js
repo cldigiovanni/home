8b69fa2308145e7e5939e259391c480e6d6c5130
Charles Digiovanni (RIT Student) <cld3566@rit.edu>
2024-11-22 17:13:48 -0500

    infinite scroll effect

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
 let elem = document.getElementById("text");
 let header = document.querySelectorAll("header")[0];
-// 13rem * 16px * 15 lines
-let line_size = 13 * 16 * 15;
-setInterval(function() {
-    // console.log(elem.scrollTop);
-    header.innerText = elem.scrollTop.toString();
 
+// 13rem * 16px * 15 lines
+let line_size = 13 * 16 * 19;
+setInterval(function() {
     elem.scrollTop += 1;
     if (elem.scrollTop > line_size)
         elem.scrollTop = 1;
 
-}, 1000 / 60)
-
+}, 1000 / 60);
